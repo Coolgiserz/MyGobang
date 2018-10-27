3a251841f65701ff79e563baa758f678a317d2ca
Csuerzhuge <0106160226@csu.edu.cn>
2018-10-27 17:23:50 +0800

    五子棋

--- CoolGobang/src/com/coolcats/gobang1018/CanvasListener.java
+++ CoolGobang/src/com/coolcats/gobang1018/CanvasListener.java
@@ -7,7 +7,6 @@ import java.util.Stack;
 
 import javax.swing.JOptionPane;
 
-
 /**
  * 点击开始游戏按钮后，开始实例化该类的对象 创建棋盘数组，每当玩家或AI落子即将棋子加入到棋盘数组中，记录棋子的行列号、黑白棋
  * 
@@ -18,7 +17,7 @@ public class CanvasListener extends MouseAdapter {
 	final static int BLACK_SIDE = 1;
 	final static int WHITE_SIDE = -1;
 	private int _x, _y;// 用于记录鼠标点击的坐标
-	public static  int[][] _chesstable;// 棋子数组，其中为0代表空，1代表黑棋，2代表白棋
+	public static int[][] _chesstable;// 棋子数组，其中为0代表空，1代表黑棋，2代表白棋
 	public static int turnState = BLACK_SIDE;// 用于表示当前轮到哪一方落子，1代表黑方
 	ChessPanel _buddyChessPanel;
 	LabelPanel _buddyLabPanel;
@@ -29,9 +28,9 @@ public class CanvasListener extends MouseAdapter {
 //	public static Stack<Point> tmpchess = new Stack<>();
 
 	public CanvasListener() {
-		
 
 	}
+
 //	public void initChessTable() {
 //		System.out.println("initTable:"+this._chesstable+"。长度：");
 //		for(int i=0;i<this._chesstable.length;i++) {
@@ -44,21 +43,21 @@ public class CanvasListener extends MouseAdapter {
 	public static void initChessTable() {
 		_chesstable = new int[Chess.TABLE_ROW + 1][Chess.TABLE_COL + 1];
 
-		System.out.println("initTable:"+_chesstable+"。长度：");
-		for(int i=0;i<_chesstable.length;i++) {
-			for(int j=0;j<_chesstable.length;j++) {
+		System.out.println("initTable:" + _chesstable + "。长度：");
+		for (int i = 0; i < _chesstable.length; i++) {
+			for (int j = 0; j < _chesstable.length; j++) {
 				_chesstable[i][j] = 0;
 			}
 		}
 	}
+
 	public void setAI(AIplayer player) {
 		System.out.println(player);
 		this.ai_player = player;
 	}
+
 	/**
-	 * 处理鼠标点击事件：
-	 * 要在开始新游戏按钮被点击后才能被触发
-	 * 根据当前所选择的游戏模式处理落子逻辑
+	 * 处理鼠标点击事件： 要在开始新游戏按钮被点击后才能被触发 根据当前所选择的游戏模式处理落子逻辑
 	 * 
 	 */
 	public void mouseClicked(MouseEvent e) {
@@ -68,14 +67,13 @@ public class CanvasListener extends MouseAdapter {
 		int chessC = (_x - Chess.OriX) / Chess.INTERVAL;
 		int modR = (_y - Chess.OriY) % Chess.INTERVAL;
 		int modC = (_x - Chess.OriX) % Chess.INTERVAL;
-
 		if (modR > Chess.INTERVAL / 2) {
 			chessR++;
 		}
 		if (modC > Chess.INTERVAL / 2) {
 			chessC++;
 		}
-		if(chessC<0||chessC>Chess.TABLE_COL||chessR<0||chessR>Chess.TABLE_ROW) {
+		if (chessC < 0 || chessC > Chess.TABLE_COL || chessR < 0 || chessR > Chess.TABLE_ROW) {
 			return;
 		}
 		System.out.println(new Point(chessR, chessC));
@@ -88,16 +86,16 @@ public class CanvasListener extends MouseAdapter {
 			// 人人对战模式
 			case LabelListener.MODE_PP:
 //				System.out.println("人人对战模式开启");
-				
+
 				if (turnState == BLACK_SIDE && _chesstable[chessR][chessC] == 0) {
 					_chesstable[chessR][chessC] = 1;
 					this._buddyChessPanel.drawBlack(chessR, chessC);
-					//将棋子数组传到ChessPanel处以便对棋子进行重绘
+					// 将棋子数组传到ChessPanel处以便对棋子进行重绘
 					this._buddyChessPanel.setBuddyTable(_chesstable);
-					LabelListener._tmpchess.add(new Point(chessR,chessC));
+					LabelListener._tmpchess.add(new Point(chessR, chessC));
 					/*
 					 * 判断是否出现赢家
-					 */          
+					 */
 					boolean status = checkWin(chessR, chessC, _chesstable);
 					if (status == true) {
 //						System.out.println("黑棋赢");
@@ -105,7 +103,8 @@ public class CanvasListener extends MouseAdapter {
 					} else {
 						System.out.println("轮到白棋");
 						turnState = WHITE_SIDE;
-						_buddyLabPanel.setStatusLabel(LabelListener._mode,LabelListener._turn,LabelListener.WHITE_SIDE);
+						_buddyLabPanel.setStatusLabel(LabelListener._mode, LabelListener._turn,
+								LabelListener.WHITE_SIDE);
 //						System.exit(0);
 
 					}
@@ -113,7 +112,7 @@ public class CanvasListener extends MouseAdapter {
 				} else if (turnState == WHITE_SIDE && _chesstable[chessR][chessC] == 0) {
 					_chesstable[chessR][chessC] = 2;
 					this._buddyChessPanel.drawWhite(chessR, chessC);
-					LabelListener._tmpchess.add(new Point(chessR,chessC));
+					LabelListener._tmpchess.add(new Point(chessR, chessC));
 
 					/*
 					 * 判断是否出现赢家
@@ -127,7 +126,8 @@ public class CanvasListener extends MouseAdapter {
 						turnState = BLACK_SIDE;
 						LabelListener._turn++;
 						System.out.println(_buddyLabPanel);
-						_buddyLabPanel.setStatusLabel(LabelListener._mode,LabelListener._turn,LabelListener.BLACK_SIDE);
+						_buddyLabPanel.setStatusLabel(LabelListener._mode, LabelListener._turn,
+								LabelListener.BLACK_SIDE);
 						System.out.println("轮到黑棋");
 
 					}
@@ -140,9 +140,9 @@ public class CanvasListener extends MouseAdapter {
 			case LabelListener.MODE_PM:
 //				System.out.println("人机对战模式开启");
 				System.out.println(LabelListener.MODE_AISECOND);
-				if(LabelListener.ai_mode.equals(LabelListener.MODE_AISECOND)) {
-				//AI后手时
-					switch(LabelListener.selectedValue) {
+				if (LabelListener.ai_mode.equals(LabelListener.MODE_AISECOND)) {
+					// AI后手时
+					switch (LabelListener.selectedValue) {
 					case "权值法":
 						weightAI = new WeigthPlayer();// 实例化权值法AI对象
 
@@ -152,7 +152,7 @@ public class CanvasListener extends MouseAdapter {
 							_chesstable[chessR][chessC] = 1;
 //							LabelListener._tmpchess.add(new Point(chessR,chessC));
 
-							//将棋子数组传到ChessPanel处以便对棋子进行重绘
+							// 将棋子数组传到ChessPanel处以便对棋子进行重绘
 							this._buddyChessPanel.setBuddyTable(_chesstable);
 //							this._buddyChessPanel.paintImmediately(0, 0, 1000, 1000);
 
@@ -163,12 +163,13 @@ public class CanvasListener extends MouseAdapter {
 							if (status == true) {
 //								System.out.println("黑棋赢");
 								JOptionPane.showMessageDialog(this._buddyFrame, "黑棋胜");
-								LabelPanel.isBegin = false;//游戏状态结束
+								LabelPanel.isBegin = false;// 游戏状态结束
 
-							} else {//进入AI算法
+							} else {// 进入AI算法
 								System.out.println("轮到白棋");
 								turnState = WHITE_SIDE;
-								_buddyLabPanel.setStatusLabel(LabelListener._mode,LabelListener.selectedValue,LabelListener._turn,LabelListener.WHITE_SIDE);
+								_buddyLabPanel.setStatusLabel(LabelListener._mode, LabelListener.selectedValue,
+										LabelListener._turn, LabelListener.WHITE_SIDE);
 								System.out.println("...权值法AI思考中...");
 								weightAI.getCurrentTable(_chesstable);
 								Point pt = weightAI.CalcRC();
@@ -177,39 +178,39 @@ public class CanvasListener extends MouseAdapter {
 								_chesstable[pt.x][pt.y] = 2;
 								this._buddyChessPanel.setBuddyTable(_chesstable);
 //								this._buddyChessPanel.repaint();
-								LabelListener._tmpchess.add(new Point(pt.x,pt.y));
+								LabelListener._tmpchess.add(new Point(pt.x, pt.y));
 
 								status = checkWin(pt.x, pt.y, _chesstable);
 								if (status == true) {
 									JOptionPane.showMessageDialog(this._buddyFrame, "白棋胜");
-									LabelPanel.isBegin = false;//游戏状态结束
+									LabelPanel.isBegin = false;// 游戏状态结束
 
 								}
 
 								turnState = BLACK_SIDE;
 								LabelListener._turn++;
 							}
-							_buddyLabPanel.setStatusLabel(LabelListener._mode,LabelListener.selectedValue,LabelListener._turn,LabelListener.BLACK_SIDE);
+							_buddyLabPanel.setStatusLabel(LabelListener._mode, LabelListener.selectedValue,
+									LabelListener._turn, LabelListener.BLACK_SIDE);
 
+						}
 
-						} 
-						
 						break;
 					case "博弈树":
 						System.out.println("...博弈树AI思考中...");
-						 gameplayer = new GameTreePlayer();//实例化博弈树AI对象
-						 //玩家走棋
+						gameplayer = new GameTreePlayer();// 实例化博弈树AI对象
+						// 玩家走棋
 						if (turnState == BLACK_SIDE && _chesstable[chessR][chessC] == 0) {
 							System.out.println("黑棋");
 							_chesstable[chessR][chessC] = 1;
 							this._buddyChessPanel.drawBlack(chessR, chessC);
-							LabelListener._tmpchess.add(new Point(chessR,chessC));
+							LabelListener._tmpchess.add(new Point(chessR, chessC));
 
-							//将棋子数组传到ChessPanel处以便对棋子进行重绘
+							// 将棋子数组传到ChessPanel处以便对棋子进行重绘
 							this._buddyChessPanel.setBuddyTable(_chesstable);
-						
-							gameplayer.getPoint(new Point(chessR,chessC));
-							gameplayer.updateSet();//更新待判断集合
+
+							gameplayer.getPoint(new Point(chessR, chessC));
+							gameplayer.updateSet();// 更新待判断集合
 							gameplayer.PrintSet();
 //							System.out.println(gameplayer.GetBestPt().getX()+","+gameplayer.GetBestPt().getY());
 							/*
@@ -218,16 +219,17 @@ public class CanvasListener extends MouseAdapter {
 							boolean status = checkWin(chessR, chessC, _chesstable);
 							if (status == true) {
 								JOptionPane.showMessageDialog(this._buddyFrame, "黑棋胜");
-							} else {//进入AI算法
+							} else {// 进入AI算法
 								System.out.println("轮到白棋");
 								turnState = WHITE_SIDE;
-								_buddyLabPanel.setStatusLabel(LabelListener._mode,LabelListener.selectedValue,LabelListener._turn,LabelListener.WHITE_SIDE);
+								_buddyLabPanel.setStatusLabel(LabelListener._mode, LabelListener.selectedValue,
+										LabelListener._turn, LabelListener.WHITE_SIDE);
 								gameplayer.getCurrentTable(_chesstable);
 								Point pt = gameplayer.GetBestPt();
 								_chesstable[pt.x][pt.y] = 2;
 								this._buddyChessPanel.drawWhite(pt.x, pt.y);
 								this._buddyChessPanel.setBuddyTable(_chesstable);
-								LabelListener._tmpchess.add(new Point(pt.x,pt.y));
+								LabelListener._tmpchess.add(new Point(pt.x, pt.y));
 
 								status = checkWin(pt.x, pt.y, _chesstable);
 								if (status == true) {
@@ -238,19 +240,19 @@ public class CanvasListener extends MouseAdapter {
 								turnState = BLACK_SIDE;
 								LabelListener._turn++;
 							}
-							_buddyLabPanel.setStatusLabel(LabelListener._mode,LabelListener.selectedValue,LabelListener._turn,LabelListener.BLACK_SIDE);
+							_buddyLabPanel.setStatusLabel(LabelListener._mode, LabelListener.selectedValue,
+									LabelListener._turn, LabelListener.BLACK_SIDE);
 
-
-						} 
+						}
 						break;
 					case "机器学习":
 						break;
 					}
-				}else if(LabelListener.ai_mode.equals(LabelListener.MODE_AIFIRST)) {
-				//AI先手时
-					
+				} else if (LabelListener.ai_mode.equals(LabelListener.MODE_AIFIRST)) {
+					// AI先手时
+
 				}
-			
+
 				break;
 			}
 		}
@@ -298,10 +300,10 @@ public class CanvasListener extends MouseAdapter {
 	 * @return
 	 */
 	public boolean checkWin(int chessR, int chessC, int[][] chesstable) {
-		int count_row = checkRow(chessR, chessC, chesstable);
-		int count_col = checkCol(chessR, chessC, chesstable);
-		int count_right_down = checkRightDown(chessR, chessC, chesstable);
-		int count_left_down = checkLeftDown(chessR, chessC, chesstable);
+		int count_row = checkRow(chessR, chessC, chesstable);// 判断一行的方向中是否出现了五子连珠
+		int count_col = checkCol(chessR, chessC, chesstable);// 判断一列的方向中是否出现了五子连珠
+		int count_right_down = checkRightDown(chessR, chessC, chesstable);// 判断斜向右下或左上的方向中是否出现了五子连珠
+		int count_left_down = checkLeftDown(chessR, chessC, chesstable);// 判断斜向右上或左下的方向中是否出现的五子连珠
 		if (count_row >= 5 || count_col >= 5 || count_right_down >= 5 || count_left_down >= 5) {
 			return true;
 		}
@@ -313,7 +315,7 @@ public class CanvasListener extends MouseAdapter {
 //		System.out.println(chesstable.length);
 		try {
 			for (int i = col + 1; i < Chess.TABLE_COL; i++) {
-				if (chesstable[row][i]==chesstable[row][col]) {
+				if (chesstable[row][i] == chesstable[row][col]) {
 					count++;
 //					System.out.println("状态->：" + chesstable[row][col].type + " " + "：" + count);
 
@@ -327,7 +329,7 @@ public class CanvasListener extends MouseAdapter {
 		}
 		try {
 			for (int i = col - 1; i >= 0; i--) {
-				if (chesstable[row][i]==chesstable[row][col]) {
+				if (chesstable[row][i] == chesstable[row][col]) {
 					count++;
 //					System.out.println("状态<-：" + chesstable[row][col].type + " " + "：" + count);
 
@@ -347,7 +349,7 @@ public class CanvasListener extends MouseAdapter {
 //		System.out.println(chesstable.length);
 		try {
 			for (int i = row + 1; i < Chess.TABLE_ROW; i++) {
-				if (chesstable[i][col]==chesstable[row][col]) {
+				if (chesstable[i][col] == chesstable[row][col]) {
 					count++;
 //					System.out.println("状态->：" + chesstable[row][col].type + " " + "：" + count);
 
@@ -361,7 +363,7 @@ public class CanvasListener extends MouseAdapter {
 		}
 		try {
 			for (int i = row - 1; i >= 0; i--) {
-				if (chesstable[i][col]==chesstable[row][col]) {
+				if (chesstable[i][col] == chesstable[row][col]) {
 					count++;
 //					System.out.println("状态<-：" + chesstable[row][col].type + " " + "：" + count);
 
@@ -378,39 +380,37 @@ public class CanvasListener extends MouseAdapter {
 	public static int checkRightDown(int row, int col, int[][] chesstable) {
 		int count = 1;
 //		System.out.println(chesstable.length);
-	
-			for (int i = 1; i < 5; i++) {
-				if (Math.max(row + i, col + i) > Chess.TABLE_ROW) {
+
+		for (int i = 1; i < 5; i++) {
+			if (Math.max(row + i, col + i) > Chess.TABLE_ROW) {
 //					System.out.println("状态中断：" + chesstable[row][col].type + " " + "：" + count);
 
-					break;
-				}
-				if (chesstable[row + i][col + i]==chesstable[row][col]) {
-					count++;
+				break;
+			}
+			if (chesstable[row + i][col + i] == chesstable[row][col]) {
+				count++;
 //					System.out.println("状态->：" + chesstable[row][col].type + " " + "：" + count);
 
-				} else {
-					break;
-				}
+			} else {
+				break;
 			}
+		}
 
-		
-		
-			for (int i = 1; i < 5; i++) {
-				if (Math.min(row - i, col - i) < 0) {
+		for (int i = 1; i < 5; i++) {
+			if (Math.min(row - i, col - i) < 0) {
 //					System.out.println("状态中断：" + chesstable[row][col].type + " " + "：" + count);
 
-					break;
-				}
-				if (chesstable[row - i][col - i]==(chesstable[row][col])) {
-					count++;
+				break;
+			}
+			if (chesstable[row - i][col - i] == (chesstable[row][col])) {
+				count++;
 //					System.out.println("状态->：" + chesstable[row][col].type + " " + "：" + count);
 
-				} else {
-					break;
-				}
+			} else {
+				break;
 			}
-	
+		}
+
 		return count;
 	}
 
@@ -433,22 +433,21 @@ public class CanvasListener extends MouseAdapter {
 			}
 		}
 
-	
-			for (int i = 1; i < 5; i++) {
-				if (Math.min(row - i, col + i) < 0 || Math.max(row - i, col + i) > Chess.TABLE_ROW) {
+		for (int i = 1; i < 5; i++) {
+			if (Math.min(row - i, col + i) < 0 || Math.max(row - i, col + i) > Chess.TABLE_ROW) {
 //					System.out.println("状态中断：" + chesstable[row][col].type + " " + "：" + count);
 
-					break;
-				}
-				if (chesstable[row - i][col + i]==chesstable[row][col]) {
-					count++;
+				break;
+			}
+			if (chesstable[row - i][col + i] == chesstable[row][col]) {
+				count++;
 //					System.out.println("状态->：" + chesstable[row][col].type + " " + "：" + count);
 
-				} else {
-					break;
-				}
+			} else {
+				break;
 			}
-		
+		}
+
 		return count;
 	}
 }

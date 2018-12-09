530b44ae1ef5780c0d8307401d1912127d645635
Csuerzhuge <0106160226@csu.edu.cn>
2018-12-09 20:48:30 +0800

    栈溢出测试

---
+++ HelloGeo/src/com/coolcats1128/TestStackOverFlow.java
@@ -0,0 +1,10 @@
+package com.coolcats1128;
+public class TestStackOverFlow {
+
+	public static void main(String[] args) {
+		new TestStackOverFlow().test();
+	}
+	public void test() {
+		test();
+	}
+}
